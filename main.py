15cf9274767cb85353826974c822edb12c00fb39
mart1in72 <martinz877267@gmail.com>
2026-01-16 14:02:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -518,9 +518,9 @@ def main(page: ft.Page):
     page.run_task(update_unread_data)
     
     # Делаем небольшую паузу для инициализации, если это Web
-    page.route("/")
+    page.route = "/"
     page.update()
 
 # Запуск приложения
 if __name__ == "__main__":
-    ft.app(target=main)
+    ft.run(main)
